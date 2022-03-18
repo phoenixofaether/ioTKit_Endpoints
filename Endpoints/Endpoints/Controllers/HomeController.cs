45d571042b646f8e96ee9d5af3b272a2a884589d
Blackbird <david.oberkalmsteiner@ost.ch>
2022-03-18 11:00:13 +0100

    working

---
+++ Endpoints/Endpoints/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Endpoints.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Endpoints.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            this.configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        public IActionResult IsUIDAllowed([FromQuery]string uID)
+        {
+            if(string.IsNullOrWhiteSpace(uID))
+            {
+                return this.BadRequest();
+            }
+
+            if("2A519929" == uID)
+            {
+                return this.Accepted();
+            }
+
+            return this.NotFound();
+        }
+    }
+}
